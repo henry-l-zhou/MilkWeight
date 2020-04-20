b0c981d361d3cef0acb4aa3c0509213889962b9a
Henry Zhou <42553559+henry-l-zhou@users.noreply.github.com>
2020-04-19 21:15:46 -0500

    gui initialized

---
+++ src/application/Main_REMOTE_15540.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author zhouh
+ *
+ */
+public class Main extends Application {
+
+  @Override
+  public void start(Stage arg0) throws Exception { // TODO Auto-generated method stub
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+	  System.out.print("Henry is big boi");
+  }
+
+}
