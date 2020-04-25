17afb07fee3344b51909aad1cfdd2f80c422a9ff
Brian Hu <bhu49@wisc.edu>
2020-04-25 19:33:51 -0400

    added input reader

--- src/backend/InputReader.java
+++ src/backend/InputReader.java
@@ -1,8 +1,30 @@
 package backend;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class InputReader {
+	private File file;
 	private List<MilkWeight> mwList;
 	
+	public InputReader(File file) {
+		this.file = file;
+		mwList = new LinkedList<MilkWeight>();
+		parseFile();
+	}
+	
+	private void parseFile() {
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if(sc != null) sc.close();
+		}
+		
+	}
 }
